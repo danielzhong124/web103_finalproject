8f51c804e8f5ddf930a31b46e4b5249d0d3d5611
Daniel Zhong <danielzhong124@gmail.com>
2026-07-15 20:27:27 -0400

    Update file structure

--- unify/vite.config.js
+++ unify/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../server/dist',
-    emptyOutDir: true,
-  },
   server: {
+    port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-      },
+      '/api': 'http://localhost:3001',
     },
   },
 })
